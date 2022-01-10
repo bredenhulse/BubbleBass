ef25b7501218dcfe447627067306b217f9990cae
bredenhulse <bredenhulse@gmail.com>
2022-01-10 07:48:56 -0600

    Yes

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<int> nums;
+    int a;
+    //Open File
+    ifstream infile;
+    infile.open("input.txt");
+    infile >> a;
+    //Loop to Make Vertex
+    for(int i = 0; i < a; i++){
+        
+    }
+}
