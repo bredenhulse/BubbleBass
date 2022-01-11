e0f3aa2abfe5839e49f322b77950dd7b5a1ede7e
Breden Hulse <91069868+bredenhulse@users.noreply.github.com>
2022-01-11 13:57:40 +0000

    Added sort loop and print statement

--- main.cpp
+++ main.cpp
@@ -5,14 +5,35 @@
 using namespace std;
 
 int main(){
-    vector<int> nums;
-    int a;
-    //Open File
-    ifstream infile;
-    infile.open("input.txt");
-    infile >> a;
-    //Loop to Make Vertex
-    for(int i = 0; i < a; i++){
-        
+  vector<int> nums;
+  int a, temp, swaps = 0;
+  //Open File
+  ifstream infile;
+  ofstream outfile;
+  infile.open("input.txt");
+  outfile.open("output");
+  infile >> a;
+  cout << a;
+  //Loop to Make Vertex
+  for(int i = 0; i < a; i++){
+      infile >> temp;
+      cout << temp << endl;
+      nums.push_back(temp);
+  }
+  //Loop To Sort It 
+  for(int i = 0; i < a; i++){
+    for(int j = i + 1; j < a; j++){
+      if(nums[j] < nums[i]){
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        swaps++;
+      }
     }
+  }
+  //Print
+  for(int i = 0; i < a; i++){
+    outfile << nums[i] << endl;
+  }
+  outfile << "\nSwaps: " << swaps;
 }
